e686d597fb0c702cdcba886fa63b2186ebf659e4
Mawada Hashim <MawadaMF@users.noreply.github.com>
2021-11-30 19:18:04 -0800

    replaced quarter filter with season
    updated cumulative distance and map filters

--- template/app.py
+++ template/app.py
@@ -24,8 +24,10 @@ data = pd.read_csv('schedule.csv')
 teams=data['opponent_name'].unique()
 
 #list of all unique quarters
-data['quarter']=pd.PeriodIndex(data.datetime, freq='Q')
-q= data['quarter'].unique()
+# data['quarter']=pd.PeriodIndex(data.datetime, freq='Q')
+# q= data['quarter'].unique()
+
+seasons=data['season'].unique()
 
 
 app.layout = html.Div([
@@ -40,11 +42,20 @@ app.layout = html.Div([
     ], style={'width': '48%', 'display': 'inline-block'}), 
     
     # top right drop menu for teams
+#     html.Div([
+#             dcc.Dropdown(
+#                 id='quarter_value',
+#                 options=[{'label': i.strftime('%YQ%q'), 'value': i.strftime('%YQ%q')} for i in q],
+#                 value='Quarter'
+#             )
+#     ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
+    
+    # top right drop menu for teams
     html.Div([
             dcc.Dropdown(
-                id='quarter_value',
-                options=[{'label': i.strftime('%YQ%q'), 'value': i.strftime('%YQ%q')} for i in q],
-                value='Quarter'
+                id='season_value',
+                options=[{'label': s, 'value': s} for s in seasons],
+                value='Season'
             )
     ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
 
@@ -62,14 +73,17 @@ app.layout = html.Div([
 # @app.callback(
 #     Output('cumdist-graphic', 'figure'),
 #     Input('team_value', 'value'),
-#     Input('quarter_value', 'value'))
+#     Input('season_value', 'value'))
 
-# def update_graph(team_value, quarter_value):
+# def update_graph(team_value, season_value):
     
-#     colors = ['gray']*len(teams)
-#     if team_value !='Teams':
-#         index=np.where(teams == team_value)[0][0]
-#         colors[index]='aqua'
+#         d=data[data['opponent_name']==team].sort_values(by='datetime').reset_index()
+# #         if quarter_value !='Quarter':
+# #             d=d[d['quarter']==quarter_value]
+#         if season_value !='Season':
+#             d=d[d['season']==season_value]
+#         dt=d['distance_traveled']
+#         cd=np.cumsum(dt)
     
 #     fig = go.Figure()
 
@@ -81,12 +95,39 @@ app.layout = html.Div([
 #         dt=d['distance_traveled']
 #         cd=np.cumsum(dt)
 
-#         fig.add_trace(go.Scatter(x = d['datetime'], 
-#                                  y = cd,
-#                                  mode = 'lines',
-#                                  name=team,
-#                                      line = dict(color = colors[i])
-#                                  ))
+# #         fig.add_trace(go.Scatter(x = d['datetime'], 
+# #                          y = cd,
+# #                          mode = 'lines',
+# #                          name=team,
+# #                              line = dict(color = colors[i])
+# #                          ))
+
+#         if team_value =='Teams' or team==team_value:
+#             fig.add_trace(
+#                 go.Scatter(x = d['datetime'], 
+#                         y = cd,
+#                         mode = 'lines',
+#                         line={'color': 'green'},
+#                         name=team,
+#                         )
+#             )
+#             fig.add_trace(
+#                 go.Scatter(x = d['datetime'], 
+#                         y = cd.where(d['result'] == 'Loss'),
+#                         mode = 'lines',
+#                         line={'color': 'red'},
+#                         name='',
+#                         )
+#             )
+#         else:
+#             fig.add_trace(
+#                 go.Scatter(x = d['datetime'], 
+#                         y = cd,
+#                         mode = 'lines',
+#                         line={'color': 'gray'},
+#                         name=team,
+#                         )
+#             )
 
 #     return fig
 
